f6669f35c77c6b99823c34be2d10242f119fda89
shahar <shaharnaim3@gmail.com>
2024-11-24 23:18:05 +0200

    add post request for handling request for recipe by name by using API

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser  from "body-parser";
+import axios from "axios";
+import dotenv from 'dotenv';
 
+dotenv.config();
+const apiKey = process.env.API_KEY;
 const app=express();
 const port=3000;
 
@@ -13,8 +17,18 @@ app.get("/",(req,res)=>{
     res.render("index.ejs");
 });
 
-app.post("/recipe",(req,res)=>{
-    console.log(req.body);
+app.post("/recipe",async (req,res)=>{
+    try {
+        const reuslt= await axios.get("https://api.api-ninjas.com/v1/recipe?query="+req.body.data,{
+            headers:{
+                'X-Api-Key':apiKey
+            }
+        });
+        console.log(reuslt.data[0].ingredients);
+    } catch (error) {
+        console.log(error.message);
+        
+    }
 
 });
 
