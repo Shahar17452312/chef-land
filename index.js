f31827f63197bcfd8d1c02f931026514964b8638
shahar <shaharnaim3@gmail.com>
2024-11-24 20:45:08 +0200

    add styling and beginning of taking care of recipe request

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from "express";
+import bodyParser  from "body-parser";
 
 const app=express();
 const port=3000;
 
 
 app.use(express.static("public"));
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
-})
+});
+
+app.post("/recipe",(req,res)=>{
+    console.log(req.body);
+
+});
+
+
+
 
 app.listen(port,(req,res)=>{
     console.log("Server is running on port "+port);
-})
+});
